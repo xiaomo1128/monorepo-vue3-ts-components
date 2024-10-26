4f13c1e34194585a31da9c73274668fff359c82b
xiaomo1128 <493559026@qq.com>
2024-10-27 00:11:52 +0800

    icon组件处理+样式处理

--- play/src/App.vue
+++ play/src/App.vue
@@ -1,13 +1,26 @@
+<!--
+ * @Author: xiaomo1128 493559026@qq.com
+ * @Date: 2024-10-22 23:54:30
+ * @LastEditors: xiaomo1128 493559026@qq.com
+ * @LastEditTime: 2024-10-27 00:10:50
+ * @FilePath: \monorepo-demo\play\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <script setup lang="ts">
-import Icon from '@zi-shui/components/icon/src/icon.vue'
+import { AddCircle } from '@vicons/ionicons5'
+// import Icon from '@zi-shui/components/icon/src/icon.vue'
 
-console.log(Icon);
+// console.log(Icon);
 
 </script>
 
 <template>
   <z-icon :color="'red'" :size="20">
-    world
+    <AddCircle></AddCircle>
+  </z-icon>
+
+  <z-icon :color="'blue'" :size="20">
+    <AddCircle></AddCircle>
   </z-icon>
 </template>
 
